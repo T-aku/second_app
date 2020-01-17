185b35ed1a386edcb8ddd040c33fe4ee54629ca7
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-17 06:35:31 +0000

    Setting password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_212728) do
+ActiveRecord::Schema.define(version: 2020_01_17_060604) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_212728) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
